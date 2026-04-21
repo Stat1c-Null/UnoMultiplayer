c3083276a473df509d15e75651f10e40e785d838
Mikita Slabysh <slabysh2015@gmail.com>
2026-04-21 19:55:01 -0400

    fixed card hover bug

--- Assets/Scripts/UI/CardUI.cs
+++ Assets/Scripts/UI/CardUI.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 /// <summary>
 /// Attach this to the Card prefab. Displays the correct sprite for a given CardData
@@ -9,16 +10,51 @@ using UnityEngine.UI;
 /// The Image component on this same GameObject will be set to the card's sprite.
 /// </summary>
 [RequireComponent(typeof(Image))]
-public class CardUI : MonoBehaviour
+public class CardUI : MonoBehaviour,IPointerEnterHandler,IPointerExitHandler
 {
     /// <summary>The card data this visual represents.</summary>
     public CardData Card { get; private set; }
 
     private Image image;
+    private RectTransform rectTransform;
+    private bool isHovered;
+    private float baseY;
+
+    public float speed = 10f;
+    public float height = 20f;
+    public Vector3 originalPosition;
 
     private void Awake()
     {
         image = GetComponent<Image>();
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    void Start()
+    {
+        originalPosition = rectTransform.localPosition;
+        baseY = originalPosition.y;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        isHovered = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        isHovered = false;
+    }
+
+    void Update()
+    {
+        Vector3 current = rectTransform.localPosition;
+        float targetY = isHovered ? baseY + height : baseY;
+        float nextY = Mathf.Lerp(current.y, targetY, Time.deltaTime * speed);
+        rectTransform.localPosition = new Vector3(current.x, nextY, current.z);
+
+        // Keep debug/public state in sync with the card's current slot.
+        originalPosition = new Vector3(current.x, baseY, current.z);
     }
 
     /// <summary>
